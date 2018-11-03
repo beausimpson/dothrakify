78c4f01df310551265db1179b5991c42b435381c
beausimpson <simpson.beau@gmail.com>
2018-11-03 11:41:57 -0500

    added translation api callback to main.js

--- javascript/main.js
+++ javascript/main.js
@@ -10,27 +10,36 @@ let guess = 0;
 let count = 0;
 
 // temporary variable to test songpull function -- will need to be removed
-var song ="hey ya"
+var song = "hey ya"
 
 function songPull(song) {
     //api for songPull here
+    // variable to hold url to bypass CORS restrictions
     var corsBypass = "https://cors-escape.herokuapp.com/"
+    // URL for to get music lyrics from selected song
     var queryURL = `${corsBypass}http://api.musixmatch.com/ws/1.1/track.search?q_track=${song}&apikey=19235e8ed115f81044447a46c258f431`;
 
+    // ajax call to get song lyrics
     $.ajax({
         url: queryURL,
         method: "GET"
+        // first callback gets the song id to be passed into secojnd ajax call
     }).then(function (response) {
         // console.log(queryURL);
+        // response returns as a string -- json.parse changes into an object
         var JSONresponse = JSON.parse(response);
         console.log(JSONresponse.message.body.track_list[0].track.track_id);
+        // trackid number that will be passed into second ajax call
         var trackId = JSONresponse.message.body.track_list[0].track.track_id;
 
+        // second ajax callback takes track id to get lyrics for chosen song
         $.ajax({
-            url: `${corsBypass}http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=19235e8ed115f81044447a46c258f431`,
+            url: `${corsBypass}https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=19235e8ed115f81044447a46c258f431`,
             method: "GET"
         }).then(function (secondResponse) {
+            // response returns as a string -- json.parse changes into an object
             var trackIdResponse = JSON.parse(secondResponse);
+            // response returns lyrics to pe passed into translation function
             var lyrics = trackIdResponse.message.body.lyrics.lyrics_body
             // console.log(lyrics)
         })
@@ -38,13 +47,29 @@ function songPull(song) {
     return lyrics
 }
 
+// temporary variable for language
+var language = "dothraki"
+
+// language parameter should only be needed if we want add feature of changing languages
 function songTranslate(lyrics, language) {
     let translation
     //api for translating song here
+    $.ajax({
+        url: `https://api.funtranslations.com/translate/${language}.json?text=${lyrics}&api_key=JU188G9Hg1LzLKqX6gSA6QeF`,
+        method: "Get"
+    }).then(function (lyricResponse) {
+        // console.log(lyricResponse)
+        // response returns translated lyrics
+        console.log(lyricResponse.contents.translated)
+        // varaible to hold translated lyrics string
+        translation = lyricResponse.contents.translated
+    })
     return translation
 
 }
 
+// will need to limit the number of characters displayed from translated string to remove "not for commercial use" at the end of string
+// possibly substring method
 function displayLyrics(translation) {
     $(".translLyrics").text(translation);
 
